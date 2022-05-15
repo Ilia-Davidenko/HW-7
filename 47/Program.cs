cf8e59853e590887b5b744aa393a344935bcbbbd
Davidenko Ilia <Ilia_d@outlook.com>
2022-05-15 17:03:40 +0300

    47, 50, 52

---
+++ 47/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+Console.Clear();
+
+double[,] result = new double[5, 5];
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        result[i, j] = new Random().NextDouble() * new Random().Next(-10,10);
+        Console.Write("{0,5:F1} ", result[i, j]);
+    }
+    Console.WriteLine();
+}
---
+++ 50/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в 
+// двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+Console.Clear();
+
+int[,] startArray = GetRandomArray(5, 5, 0, 10);
+PrintArray(startArray);
+//Console.WriteLine(SearchNumber(startArray, 3));
+
+Console.WriteLine("Введите число для поиска");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < startArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < startArray.GetLength(1); j++)
+        {
+            if (startArray[i, j] == number)
+            {
+                Console.WriteLine($"{number} находится в строке {i} столбце {j}");
+            }
+//            else Console.WriteLine($"{number} отсутствует в массиве");
+        }
+    }
+
+
+int[,] GetRandomArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// int SearchNumber(int[,] array, int number)
+// {
+//     int match = 0;
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if (array[i, j] == number)
+//             {
+//                 match = array[i, j];
+//                 Console.WriteLine($"{number} находится в строке {i} столбце {j}");
+//             }
+//         }
+//     }
+//     return match;
+// }
